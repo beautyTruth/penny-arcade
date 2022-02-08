f991a580872508c1a1f977c114dccea1c96fff56
Phil <happy.melodist@gmail.com>
2022-02-08 07:55:19 -0600

    fix bugs in css and JavaScript predesign files

--- app.js
+++ app.js
@@ -15,26 +15,26 @@
 
 // navbar code
 
-// const navColors = ["#EB92BE", "#FFEF78", "#A8E7E9", "#B1FFFD", "#77D970"];
+const navColors = ["#EB92BE", "#FFEF78", "#A8E7E9", "#B1FFFD", "#77D970"];
 
-// let i = 0;
+let i = 0;
 
-// Array.from(document.querySelectorAll(".nav-link")).forEach((tummy) => {
-//   tummy.style.cssText = `background-color: ${navColors[i++]}`;
-// });
+Array.from(document.querySelectorAll(".nav-link")).forEach((tummy) => {
+  tummy.style.cssText = `background-color: ${navColors[i++]}`;
+});
 
 // // end of navbar code
 
 // // code for change class
 
-// const container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-// document.querySelector(".open-navbar-icon").addEventListener("click", () => {
-//   container.classList.add("change");
-// });
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
 
-// document.querySelector(".close-navbar-icon").addEventListener("click", () => {
-//   container.classList.remove("change");
-// });
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
 
 // end of code for change class
