9cfafd7054d4706463a3f30e5181a3e28a462a82
Phil <happy.melodist@gmail.com>
2022-02-08 07:27:12 -0600

    set up pre-design code and obtain all elements for the navbar, header, and section one

---
+++ app.js
@@ -0,0 +1,40 @@
+// document.querySelector(".hamburger-menu").addEventListener("click", () => {
+//   document.querySelector(".container").classList.toggle("change");
+// });
+
+// const newYear = document.querySelector(".year");
+// const yearUpdate = new Date().getFullYear();
+// newYear.textContent = yearUpdate;
+
+// document.querySelector(".scroll-btn").addEventListener("click", () => {
+//   document.querySelector("html").style.scrollBehavior = "smooth";
+//   setTimeout(() => {
+//     document.querySelector("html").style.scrollBehavior = "unset";
+//   }, 1000);
+// });
+
+// navbar code
+
+// const navColors = ["#EB92BE", "#FFEF78", "#A8E7E9", "#B1FFFD", "#77D970"];
+
+// let i = 0;
+
+// Array.from(document.querySelectorAll(".nav-link")).forEach((tummy) => {
+//   tummy.style.cssText = `background-color: ${navColors[i++]}`;
+// });
+
+// // end of navbar code
+
+// // code for change class
+
+// const container = document.querySelector(".container");
+
+// document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+//   container.classList.add("change");
+// });
+
+// document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+//   container.classList.remove("change");
+// });
+
+// end of code for change class
